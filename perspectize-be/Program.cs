9fed47a0a91b460c33b0b5bf1d50138b9812893d
James Jordan <jjordan@iralogix.com>
2025-04-28 17:47:29 -0400

    feat: initial table and migrations set up

--- perspectize-be/Program.cs
+++ perspectize-be/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using perspectize_be.Data;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
+// Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
